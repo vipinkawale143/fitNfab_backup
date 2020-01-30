39739a57a0b0f18255a05a2c1dc561f90774c612
vipin kawale <vkawale143@gmail.com>
2020-01-30 12:22:02 +0530

    second Commit

---
+++ login/Controllers/ForgetPasswordController.cs
@@ -0,0 +1,141 @@
+﻿using login.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace login.Controllers
+{
+    public class ForgetPasswordController : Controller
+    {
+        // GET: ForgetPassword
+
+
+            public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+        public ActionResult InsertUserName(string username)
+        {
+            
+
+            Session["id"] = username;
+
+            SqlConnection con2 = new SqlConnection(@"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=project;Integrated Security=True");
+            con2.Open();
+
+            SqlCommand cmd2 = new SqlCommand();
+            cmd2.Connection = con2;
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = "select Type from Users where UserName=@username";
+
+
+            cmd2.Parameters.AddWithValue("username", username);
+
+            var type = cmd2.ExecuteScalar().ToString();
+            if (type == null)
+            {
+                Response.Write("< script >alert(Otp not match)  </script>");
+                return RedirectToAction("Login", "LoginId");
+            }
+            con2.Close();
+
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=project;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+
+            if (type.Equals("Client"))
+            {
+                cmd.CommandText = "select Phone from Client where Email=@username";
+            }
+            else
+            {
+                cmd.CommandText = "select Phone from Owner where Email=@username";
+            }
+
+            cmd.Parameters.AddWithValue("@username", username);
+
+            var number = cmd.ExecuteScalar().ToString();
+            con.Close();
+
+
+            string apikey = ConfigurationManager.AppSettings["apiKey"].ToString();
+
+            var status = "";
+            Random ran = new Random();
+            int otp = ran.Next(1000, 9999);
+
+            TempData["otp"] = otp;
+
+            string msg = " OTP :" + otp;
+            string enCode = HttpUtility.UrlEncode(msg);
+
+            using (var webClient = new WebClient())
+            {
+
+                byte[] res = webClient.UploadValues("https://api.textlocal.in/send/", new System.Collections.Specialized.NameValueCollection() {
+
+                     {"apiKey",apikey},
+                     {"numbers" ,number},
+                     {"message",enCode },
+                     { "sender","TXTLCL"} });
+
+                string result = System.Text.Encoding.UTF8.GetString(res);
+
+                var jsonObj = JObject.Parse(result);
+                status = jsonObj["status"].ToString();
+
+            }
+            return View();
+        }
+
+        public ActionResult Verify(int otp=1100)
+        {
+
+            if (otp == Convert.ToInt64(TempData["otp"]))
+            {
+                return RedirectToAction("UpdatePassword","ForgetPassword");
+            }
+            else
+            {
+                Response.Write("< script >alert(Otp not match)  </script>");
+            }
+            return RedirectToAction("Login", "LoginId");
+        }
+
+        public ActionResult UpdatePassword()
+        {
+            return View();
+        }
+        public ActionResult EditPassWord(string pass)
+        {
+            EncryptDecrypt e = new EncryptDecrypt();
+            string id = Session["id"].ToString();
+            SqlConnection con2 = new SqlConnection(@"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=project;Integrated Security=True");
+            con2.Open();
+
+            SqlCommand cmd2 = new SqlCommand();
+            cmd2.Connection = con2;
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = "update Users set Password=@Pass where UserName=@id";
+
+            cmd2.Parameters.AddWithValue("id", id);
+            cmd2.Parameters.AddWithValue("pass", e.Base64Encode(pass));
+
+            cmd2.ExecuteNonQuery();
+            con2.Close();
+
+            return RedirectToAction("Login", "LoginId");
+
+        }
+    }
+}
---
+++ login/Controllers/LogoutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace login.Controllers
+{
+    public class LogoutController : Controller
+    {
+        // GET: Logout
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            Session.Clear();
+            return RedirectToAction("Login","LoginId");
+        }
+    }
+}
